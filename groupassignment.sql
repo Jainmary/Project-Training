1e60e1e3f2d42c4284b07afade0730c5480d0aa5
Jainmary <64886205+Jainmary@users.noreply.github.com>
2020-05-08 06:45:01 +0530

    Update groupassignment.sql

--- groupassignment.sql
+++ groupassignment.sql
@@ -1,3 +1,7 @@
 create database assignment;
 use assignment;
 create table address(id int auto_increment not null primary key, address_line1 varchar(50) null, address_line2 varchar(50) null, city varchar(25) null, district varchar(25) null, state varchar(20) null, pincode int not null, contact_person varchar(50) null, contact_num int not null);
+insert into address (id, address_line1, address_line2, city, district, state, pincode, contact_person, contact_number) value (1, mariagiri, , kaliakkavilai, kaliakkavilai, kanyakumari, tamil nadu, 629153, tom, 76838975394 );
+insert into address (id, address_line1, address_line2, city, district, state, pincode, contact_person, contact_number) value (2, xxxxxxxx, xxxxxxx, kaliakkavilai, kanyakumari, tamilnadu, 629172, geetha, xxxxxxxxx);
+insert into address (id, address_line1, address_line2, city, district, state, pincode, contact_person, contact_number) value (3, xxxxxxxx, xxxxxxxxx, Marthandam, Kanyakumari, tamil nadu, 629175, jerry, xxxxxxxx);
+select * from address
